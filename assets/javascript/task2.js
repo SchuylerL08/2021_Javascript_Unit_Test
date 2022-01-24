4fec217e9f2bd6b51f847edffb4d4ead46eea2ea
16074 <schuyler.lupino@vtsd.com>
2022-01-24 09:55:35 -0500

    Javascript Test

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,14 +1,16 @@
 /* Put code for the task below */
 
 function tableRed(){
-    var color = document.getElementById("table-striped");
-    color.style.display = "none";
+    var color1 = document.getElementById("task2Table");
+    color1.style.backgroundColor = "red";
 }
 
 function tableBlue(){
-
+    var color2 = document.getElementById("task2Table");
+    color2.style.backgroundColor = "blue";
 }
 
 function tableYellow(){
-
+    var color3 = document.getElementById("task2Table");
+    color3.style.backgroundColor = "yellow";
 }
