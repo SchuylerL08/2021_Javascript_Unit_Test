58b5376f2569a02b88c901316d01cfd6e1fd02a2
16074 <schuyler.lupino@vtsd.com>
2022-01-25 09:24:13 -0500

    Javascript Test

--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -1,8 +1,12 @@
 /* Put code for the task below */
 
+/* Changes directions text to "I Clicked It!" */
 function changeDirections(){
+    /* Makes directions invisible */
     var directions = document.getElementById("basicDirections");
     directions.style.display = "none";
+
+    /* Makes text visible */
     var newText = document.getElementById("directionsReplacement");
     newText.style.display = "block";
 }
--- assets/javascript/task4.js
+++ assets/javascript/task4.js
@@ -1,1 +1,11 @@
 /* Put code for the task below */
+
+/* Opens a new tab with "New page has been created" text */
+function clearPage() {
+    window.open(
+        "newPage.html", "_blank"
+    )
+}
+
+/* I found this code from geeksforgeeks.org on https://www.geeksforgeeks.org/how-to-open-url-in-new-tab-using-javascript/
+*  The code opens a new tab in the same window using a specific link */
